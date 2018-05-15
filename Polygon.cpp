2a8c63c2183b065bcfc4288e4e0ad14ba01756a2
pksiazek <piotr.ksiazek@zoho.com>
2018-05-15 21:49:37 +0200

    Add documentation, refactor, add const qualifiers

--- Polygon.cpp
+++ Polygon.cpp
@@ -1,15 +1,15 @@
 //
-// Created by HyperWorks on 2018-05-09.
+// Created by PiotrKsiazek 2018-05-09.
 //
 
 #include <cmath>
 #include <iostream>
 #include "Polygon.h"
 
-const float EPS_2 = 0.0001;
+//const float EPS_2 = 0.0001;
 
-bool Polygon::contains(Vertex a) {
-    bool res = contains_faster(a.getPoint());
+bool Polygon::contains(const Vertex &a) {
+    bool res = contains_wn_improved(a.getPoint());
     if (res) {
         tags.insert(a.getIndex());
     }
@@ -17,8 +17,8 @@ bool Polygon::contains(Vertex a) {
 }
 
 
-bool Polygon::contains(index_t id, Point p) {
-    bool res = contains_faster(p);
+bool Polygon::contains(const index_t id, const Point &p) {
+    bool res = contains_wn_improved(p);
     if (res) {
         tags.insert(id);
     }
@@ -26,7 +26,7 @@ bool Polygon::contains(index_t id, Point p) {
 }
 
 
-bool Polygon::contains_wn(Point p) {
+bool Polygon::contains_wn(const Point &p) {
     auto size = vertices.size();
     if (size == 0) return false;
     real_t sum = 0;
@@ -42,30 +42,35 @@ bool Polygon::contains_wn(Point p) {
     return fabs(fabs(sum) - 2 * PI) < EPS;
 }
 
-bool Polygon::contains_faster(Point p) {
+bool Polygon::contains_wn_improved(const Point &p) {
     int wn = 0;    // the  winding number counter
     int size = vertices.size();
     real_t le;
-    // loop through all edges of the polygon
+
+                                 // loop through all edges of the polygon
     for (int i = 0; i < size - 1; i++) {   // edge from V[i] to  V[i+1]
         auto v1 = vertices[i].getPoint();
-        if(v1==p) return true;
+        if(v1==p)
+            return true;
+
         auto v2 = vertices[i + 1].getPoint();
-        if(v2==p) return true;
-        if (v1.y <= p.y) {          // start y <= P.y
+        if(v2==p)
+            return true;
+
+        if (v1.y <= p.y) {       // start y <= P.y
             if (v2.y > p.y) {    // an upward crossing
                 le = isLeft(v1, v2, p);
-                if (le > EPS_2)  // P left of  edge
-                    ++wn;            // have  a valid up intersect
-                else if (fabs(le) < EPS_2)
+                if (le > EPS)  // P left of  edge
+                    ++wn;        // have  a valid up intersect
+                else if (fabs(le) < EPS)
                     return true;
             }
-        } else {                        // start y > P.y (no test needed)
-            if (v2.y <= p.y) {     // a downward crossing
+        } else {                 // start y > P.y (no test needed)
+            if (v2.y <= p.y) {   // a downward crossing
                 le = isLeft(v1, v2, p);
-                if (le < -EPS_2)  // P right of  edge
-                    --wn;            // have  a valid down intersect
-                else if (fabs(le) < EPS_2) {
+                if (le < -EPS) // P right of  edge
+                    --wn;        // have  a valid down intersect
+                else if (fabs(le) < EPS) {
                     return true;
                 }
             }
@@ -76,4 +81,8 @@ bool Polygon::contains_faster(Point p) {
 
 const std::set<index_t> &Polygon::getTags() const {
     return tags;
+}
+
+void Polygon::reset() {
+    tags.clear();
 }
--- Polygon.h
+++ Polygon.h
@@ -1,5 +1,5 @@
 //
-// Created by HyperWorks on 2018-05-09.
+// Created by PiotrKsiazek on 2018-05-09.
 //
 
 #ifndef DAFTCODE_POLYGON_H
@@ -15,14 +15,25 @@ private:
     const index_t id;
     std::vector<Vertex> vertices;
     std::set<index_t> tags;
-public:
-    const std::set<index_t> &getTags() const;
 
-private:
+    /**
+     * Standard implementation of WN algorithm,
+     * from Competitive Programming 3
+     * @param[in] p : 2D point
+     * @return true iff the polygon contains the point p
+     */
+    bool contains_wn(const Point &p);
 
-    bool contains_wn(Point p);
-
-    bool contains_faster(Point p);
+    /**
+     * Alternative implementation of WN algorithm
+     * believed to have a better performance
+     * it also catches points on the edges
+     * http://geomalgorithms.com/a03-_inclusion.html
+     * https://stackoverflow.com/questions/37703202/winding-number-algorithm-and-point-on-boundary-edge-of-convex?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+     * @param[in] p : 2D point
+     * @return true if the polygon contains the point p
+     */
+    bool contains_wn_improved(const Point &p);
 
 public:
     Polygon(index_t poly_id, std::vector<index_t> &vertices_list, std::vector<Vertex> &global_vertices) : id(poly_id),
@@ -32,18 +43,18 @@ public:
         }
     };
 
-    void print() {
-        for (auto v : vertices) {
-            printf("%.2Lf|%.2Lf ", v.getPoint().x, v.getPoint().y);
-        }
-        printf("\n");
-    }
+    bool contains(const Vertex &a);
 
-    bool contains(Vertex a);
-
-    bool contains(index_t id, Point p);
+    bool contains(index_t id, const Point &p);
 
     index_t getId() { return id; }
+
+    const std::set<index_t> &getTags() const;
+
+    /**
+     * Reset polygon's tag set
+     */
+    void reset();
 };
 
 
